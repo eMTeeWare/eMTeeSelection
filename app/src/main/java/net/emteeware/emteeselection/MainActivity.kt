f39fda0ce565fdc614ba3bca5eb23aee310028d8
Merlin Thomas <emteeware@gmail.com>
2019-11-17 16:55:19 +0100

    Migrate implementation to recyclerview

--- app/src/main/java/net/emteeware/emteeselection/MainActivity.kt
+++ app/src/main/java/net/emteeware/emteeselection/MainActivity.kt
@@ -1,40 +1,21 @@
 package net.emteeware.emteeselection
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_series -> {
-                message.text = ""
-                seriesList.visibility = View.VISIBLE
-                val series = arrayOf("Marvel's Luke Cage - Season 2", "Marvel's Iron Fist - Season 2", "Dexter - Season 8", "Riverdale - Season 2")
-                seriesList.adapter = ListExampleAdapter(this, series)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_people -> {
-                seriesList.visibility = View.INVISIBLE
-                message.setText(R.string.title_people)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_selections -> {
-                seriesList.visibility = View.INVISIBLE
-                message.setText(R.string.title_selections)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    val shows = arrayListOf("Marvel's Luke Cage - Season 2", "Marvel's Iron Fist - Season 2", "Dexter - Season 8", "Riverdale - Season 2")
+    private lateinit var layoutManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        layoutManager = LinearLayoutManager(this)
+        rvSelecteeList.layoutManager = layoutManager
+        rvSelecteeList.adapter = SelecteeAdapter(this, shows)
     }
 }
---
+++ app/src/main/java/net/emteeware/emteeselection/SelecteeAdapter.kt
@@ -0,0 +1,42 @@
+package net.emteeware.emteeselection
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout.view.*
+
+/**
+ * Created by Merlin Thomas on 19.12.2017.
+ * www.emteeware.net
+ */
+class SelecteeAdapter(private val context: MainActivity, private val selecteeList: ArrayList<String>)
+    : RecyclerView.Adapter<SelecteeAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(context).inflate(
+                        R.layout.layout,
+                        parent,
+                        false
+                )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return selecteeList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.selecteeName?.text = selecteeList.get(position)
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, (selecteeList.get(position)).plus(" selected "), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val selecteeName = view.tvSelecteeName
+    }
+
+
+}
